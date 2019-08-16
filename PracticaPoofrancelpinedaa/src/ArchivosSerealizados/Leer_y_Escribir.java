8b730a577b16b82a9ae5b69727cef54e4d4f115d
ISTLOJA V <ISTLOJA V@ISTLOJA14>
2019-08-16 08:56:16 -0500

    ArchivosSerealizados

---
+++ PracticaPoofrancelpinedaa/src/ArchivosSerealizados/Leer_y_Escribir.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ArchivosSerealizados;
+
+import ArchivosSerealizados.Mascota;
+import Herencia_FamiliarPineda.Persona;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author IST.LOJA
+ */
+public class Leer_y_Escribir {
+    private static Object Objeto;
+    private Object lector;
+    
+    public void escribirObjeto(String direccion,Persona p1){
+        try{
+        ObjectOutputStream escritor=new  ObjectOutputStream( new FileOutputStream(direccion));
+        escritor.writeObject(p1);
+        escritor.close();
+        }
+        catch(Exception e){ 
+            System.out.println("se produjo un error"+e);
+            
+        }
+        
+    }
+    public void leerArchivos(String direccion) {
+        try{
+        ObjectInputStream lector=new ObjectInputStream(new FileInputStream(direccion));
+        Object Auxiliar =lector.readObject();
+        Persona p1 =(Persona) Auxiliar;
+            System.out.println(p1.getMascota().getNombre()+" su edad es "+p1.getMascota().getEdad());
+    }
+        catch (Exception e){
+            System.out.println(e);
+            
+        }
+    }
+    public List<Persona> leerPersona(String url){
+        
+    }
+    public void leerLista(String url){
+        try{
+            ObjectInputStream lector=new ObjectInputStream(new FileInputStream(url));
+            Object auxiliar = lector.readObject();
+            while (auxiliar!=null){
+                Persona p1=(Persona)auxiliar;
+                System.out.println(p1.getMascota().getNombre()+"su edad e"+p1.getMascota());
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    public void escribirlista(String direccion, List<Persona> lista){
+        try{
+        ObjectOutputStream escritor=new  ObjectOutputStream( new FileOutputStream(direccion));
+            for (Persona persona : lista) {
+                escritor.writeObject(persona);
+            }
+ escritor.close();
+        }catch (Exception e){
+            System.out.println("se ha producido un error"+e);
+        }
+    }
+    public static void main(String[] args) {
+        Leer_y_Escribir objeto =new Leer_y_Escribir();
+        Persona p2 = new Persona ("Francelito","Pineda","098365",21,new Mascota("Max",2,4));
+         Persona p1 = new Persona ("Celio","alvarado","098365",20,new Mascota("lobo",2,4));
+          Persona p3 = new Persona ("Jhon","puglla","09845365",21,new Mascota("juy",2,4));
+           Persona p4 = new Persona ("Vcente ","Luzuriaga","098365",21,new Mascota("Max",2,4));
+           List<Persona>lista=new ArrayList<>();//creamo la lista para guardar objetos
+           //un avez que creamos la lista, ponemo los objetos dentro de ella
+         lista.add(p1);
+         lista.add(p2);
+         lista.add(p3);
+         lista.add(p4);
+//         objeto.escribirlista"(‪C:\Users\ISTLOJA V\Desktop\ejenplo.txt", lista);
+        // objeto.escribirObjeto("\\Users\\IST.LOJA\\Desktop\\Ejemplo 1.txt",p2);
+       // objeto.leerArchivos("\\Users\\IST.LOJA\\Desktop\\Ejemplo 1.txt");
+         objeto.leerLista("‪C:\\Users\\ISTLOJA V\\Desktop\\ejenplo.txt");
+    }
+}
+
---
+++ PracticaPoofrancelpinedaa/src/ArchivosSerealizados/Mascota.java
@@ -0,0 +1,61 @@
+                                                                                                                         /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ArchivosSerealizados;
+
+/**
+ *
+ * @author ISTLOJA V
+ */
+public class Mascota {
+   private String nombre;
+   private int edad;
+   private int NumerodePatas;
+
+    public Mascota() {
+    }
+
+    public Mascota(String nombre, int edad, int NumerodePatas) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.NumerodePatas = NumerodePatas;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getNumerodePatas() {
+        return NumerodePatas;
+    }
+
+    public void setNumerodePatas(int NumerodePatas) {
+        this.NumerodePatas = NumerodePatas;
+    }
+    public static void main(String[] args) {
+       
+       Mascota objeto=new Mascota ();   
+        objeto.setNombre("rex");
+        objeto.setEdad(2);
+        objeto.setNumerodePatas(4);
+        System.out.println(objeto.getNombre());
+        System.out.println(objeto.getEdad());
+        System.out.println(objeto.getNumerodePatas());
+        
+        
+}
+}
